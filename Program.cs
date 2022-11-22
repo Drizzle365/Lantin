85fae97e4fdfc67dc4bd7704021b7c3b5daeba26
Drizzle <i@drizzle.vip>
2022-11-22 12:34:09 +0800

    配置文件

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 using MudBlazor.Services;
 using Lantin.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 Func<IServiceProvider, IFreeSql> freeSqlFactory = _ =>
 {
     IFreeSql freeSql = new FreeSql.FreeSqlBuilder()
-        .UseConnectionString(FreeSql.DataType.Sqlite, @"")
+        .UseConnectionString(FreeSql.DataType.MySql,
+            $"Data Source={builder.Configuration.GetSection("MySql:Host")};" +
+            $"Port={builder.Configuration.GetSection("MySql:Port")};" +
+            $"User ID={builder.Configuration.GetSection("MySql:UserName")};" +
+            $"Password={builder.Configuration.GetSection("MySql:PassWord")}; " +
+            $"Initial Catalog={builder.Configuration.GetSection("MySql:DBName")};" +
+            $"Charset=utf8; SslMode=none;Min pool size=1")
         .UseMonitorCommand(cmd => Console.WriteLine($"Sql：{cmd.CommandText}")) //监听SQL语句
         .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。
         .Build();
@@ -20,7 +27,7 @@ var app = builder.Build();
 using (IServiceScope serviceScope = app.Services.CreateScope())
 {
     var freeSql = serviceScope.ServiceProvider.GetRequiredService<IFreeSql>();
-    freeSql.CodeFirst.SyncStructure(typeof(Article)); 
+    freeSql.CodeFirst.SyncStructure(typeof(Article));
     freeSql.CodeFirst.SyncStructure(typeof(Comment));
 }
 
