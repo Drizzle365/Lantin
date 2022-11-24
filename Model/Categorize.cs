89362a56bb74788bb08e21a8662d4109dda5e0e4
Drizzle <i@drizzle.vip>
2022-11-24 22:43:08 +0800

    编辑框工具栏

---
+++ Model/Categorize.cs
@@ -0,0 +1,11 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace Lantin.Model
+{
+    public class Categorize
+    {
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public int CategorizeId { get; set; }
+        public string? Name { get; set; }
+    }
+}
