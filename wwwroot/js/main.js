d4ceab2fe59cef2f54ab0642da4e36cf04723c2c
Drizzle <i@drizzle.vip>
2022-11-24 12:06:00 +0800

    编辑器文章发布

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -3,7 +3,14 @@
 function InitEditor() {
     ClassicEditor
         .create(document.querySelector('#editor'))
+        .then(r => {
+            window.editor = r
+        })
         .catch(error => {
             console.error(error);
         });
+}
+
+function GetEditorData() {
+    return window.editor.getData()
 }
