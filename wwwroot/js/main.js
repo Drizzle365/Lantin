cff942dc2341db551a6f7a9e26ab47454108cdde
Drizzle <i@drizzle.vip>
2022-12-13 12:45:47 +0800

    去除ckeditor改用mudblazor-markdown 加入fancybox

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,25 +1,9 @@
-﻿// noinspection JSUnresolvedVariable
-
-function InitEditor() {
-    BalloonEditor
-        .create(document.querySelector('#editor'), {
-        })
-        .then(editor => {
-            window.editor = editor;
-        })
-        .catch(err => {
-            console.error(err.stack);
-        });
-}
-
-function GetEditorData() {
-    return window.editor.getData()
-}
-
-function SetEditorData(content) {
-    editor.setData(content)
-}
+﻿
 
 function InitHighlight() {
     hljs.highlightAll();
+}
+function InitFancyBox(){
+    Fancybox.bind("img", {
+    });
 }
