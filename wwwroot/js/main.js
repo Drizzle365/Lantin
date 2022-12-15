c83f156e1610824ed74a1d68a1d0a131260a340c
Drizzle <i@drizzle.vip>
2022-12-15 21:27:54 +0800

    编辑器支持Tab

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,5 +1,17 @@
-﻿
-function InitFancyBox(){
-    Fancybox.bind(".mud-markdown-body img", {
-    });
+﻿function InitFancyBox() {
+    Fancybox.bind(".mud-markdown-body img", {});
+}
+
+function EditorTab() {
+    const editor = document.getElementById("editor")
+    document.getElementById("editor").onkeydown = (e) => {
+        if (e.key === "Tab") {
+            e.preventDefault();
+            let start = editor.selectionStart;
+            let end = editor.selectionEnd;
+            editor.value = editor.value.substring(0, start) + "    " + editor.value.substring(end);
+            editor.selectionStart = start + 1;
+            editor.selectionEnd = start + 1;
+        }
+    }
 }
